c65ca65329a6a95ebe95ac6423f45320f870525b
adalbertus <wojciech@pietkiewicz.pl>
2020-06-21 13:14:10 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+let sensorLeft = 0
+let sensorRight = 0
+let robotMode = "waiting"
+
+pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P16, PinPullMode.PullUp)
+
 function driveBackward (ms: number = 0) {
     pins.servoWritePin(AnalogPin.P1, 180)
     pins.servoWritePin(AnalogPin.P2, 0)
@@ -68,7 +75,53 @@ function turn(direction : Direction, ms : number = 100) {
     driveStop()
 }
 
+function readSensors() {
+    sensorRight = pins.digitalReadPin(DigitalPin.P15)
+    sensorLeft = pins.digitalReadPin(DigitalPin.P16)
+}
+
+function turnUntilSensor(direction: Direction){
+    turn(direction)
+    readSensors()
+
+    if(direction == Direction.Right) {
+        if(sensorRight == 0) {
+            turn(Direction.Right)
+        }
+    } else {
+        if(sensorLeft == 0) {
+            turn(Direction.Left)
+        }       
+    }
+    pause(50)
+}
+
+function followTheLine() {
+    readSensors()
+    
+    if (sensorRight == 1 && sensorLeft == 1) { // move forward
+    	driveForward() 
+    } else if (sensorRight == 1 && sensorLeft == 0) { // turn left
+    	turnUntilSensor(Direction.Left)
+    } else if (sensorRight == 0 && sensorLeft == 1) { // turn right
+    	turnUntilSensor(Direction.Right)    	
+    } else { // stop
+        driveStop()
+    }
+}
+
+input.onButtonPressed(Button.A, function () {
+    pause(100)
+    robotMode = "followTheLine"
+})
+
+input.onButtonPressed(Button.B, function () {
+    robotMode = "STOP"
+    driveStop()
+})
 
 basic.forever(function () {
-	
+    if(robotMode == "followTheLine") {
+        followTheLine()
+    }	
 })
